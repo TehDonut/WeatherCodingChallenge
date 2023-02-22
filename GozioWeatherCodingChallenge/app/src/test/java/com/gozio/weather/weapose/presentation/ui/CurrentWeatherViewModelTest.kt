177d786cf3d86e865ee8b7730b592f7b6bf46a8b
DESKTOP-CDFP0E9\Shots <shim919@gmail.com>
2023-02-22 17:06:43 -0500

    attempted to add unit test for viewmodel

--- GozioWeatherCodingChallenge/app/src/test/java/com/gozio/weather/weapose/presentation/ui/CurrentWeatherViewModelTest.kt
+++ GozioWeatherCodingChallenge/app/src/test/java/com/gozio/weather/weapose/presentation/ui/CurrentWeatherViewModelTest.kt
@@ -2,11 +2,19 @@ package com.gozio.weather.weapose.presentation.ui
 
 import android.content.Context
 import com.gozio.weather.weapose.base.BaseTest
+import com.gozio.weather.weapose.data.model.CurrentWeather
+import com.gozio.weather.weapose.data.repository.WeatherRepository
 import com.gozio.weather.weapose.presentation.model.CurrentWeatherMapper
 import com.gozio.weather.weapose.presentation.ui.home.CurrentWeatherViewModel
+import io.mockk.MockKAnnotations
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit4.MockKRule
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class CurrentWeatherViewModelTest : BaseTest() {
     @get:Rule
@@ -19,5 +27,29 @@ class CurrentWeatherViewModelTest : BaseTest() {
     lateinit var currentWeatherMapper: CurrentWeatherMapper
 
     private lateinit var viewModel: CurrentWeatherViewModel
+
+    @MockK
+    lateinit var weatherRepository: WeatherRepository
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init()
+        viewModel = CurrentWeatherViewModel(weatherRepository)
+    }
+
+    @Test
+    fun `test viewModel_getWeather`() {
+        every { getWeather(any(),any()) }.answers { CurrentWeather(id = 123) }
+        getWeather(123.1, 123.1)
+        assert(viewModel.currentWeather.value.id == 123)
+    }
+
+    fun getWeather(lat: Double, lon: Double) {
+        runBlocking {
+            this.launch {
+                weatherRepository.getWeather(lat, lon)
+            }
+        }
+    }
 
 }
